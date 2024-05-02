023ed63491285f763eb22cd06fe63fabf28ded94
Thomas Andersen <coolfatboy02@gmail.com>
2024-05-02 13:10:39 +0200

    I can not be redclared

--- GASLanguageProcessor/obj/Debug/net8.0/GASLanguageProcessor.AssemblyInfo.cs
+++ GASLanguageProcessor/obj/Debug/net8.0/GASLanguageProcessor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GASLanguageProcessor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b14e8a70d9e7365c009563f366da944950a43c80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0852baf540795d37ecb670ee58465afdd04ddf")]
 [assembly: System.Reflection.AssemblyProductAttribute("GASLanguageProcessor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GASLanguageProcessor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
