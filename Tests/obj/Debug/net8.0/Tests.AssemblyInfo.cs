a5874de1983da831d12290ce58a5eb8fa97fea7b
ThomasSteenAndersen <coolfatboy02@gmail.com>
2024-05-02 15:31:40 +0200

    String trim

--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0852baf540795d37ecb670ee58465afdd04ddf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023ed63491285f763eb22cd06fe63fabf28ded94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
