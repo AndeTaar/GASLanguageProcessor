3448da48a4eb6f00d0f5a673b60d9239137b7504
AndreasBM1 <113091983+AndreasBM1@users.noreply.github.com>
2024-05-06 15:18:45 +0200

    Unfinished tests for binaryOp

---
+++ Tests/OperationalSemantics/InterpreterTests/EvaluateExpressionTests/EvaluateExpressionBinaryOp.cs
@@ -0,0 +1,93 @@
+﻿using Antlr4.Runtime;
+using GASLanguageProcessor;
+using GASLanguageProcessor.AST.Expressions;
+using GASLanguageProcessor.AST.Terms;
+using GASLanguageProcessor.FinalTypes;
+using GASLanguageProcessor.TableType;
+
+namespace Tests.OperationalSemantics.InterpreterTests.EvaluateExpressionTests;
+
+public class EvaluateExpressionBinaryOp
+{
+    
+    [Fact]
+    public void PassEvaluateExpressionBinaryOpPlusNumber()
+    {
+        var visitor = new ToAstVisitor();
+        var interpreter = new Interpreter();
+        var inputStream = new AntlrInputStream(
+            "2+6");
+        var lexer = new GASLexer(inputStream);
+        var tokenStream = new CommonTokenStream(lexer);
+        var parser = new GASParser(tokenStream);
+        var expression = visitor.VisitExpression(parser.expression()) as Expression;
+        var scope = new Scope(null, null);
+        var result = interpreter.EvaluateExpression(expression, scope);
+
+
+        Assert.NotNull(result);
+        Assert.IsType<float>(result);
+        Assert.Equal((float) 8, result);
+    }
+    
+    [Fact]
+    public void PassEvaluateExpressionBinaryOpPlusString()
+    {
+        var visitor = new ToAstVisitor();
+        var interpreter = new Interpreter();
+        var inputStream = new AntlrInputStream(
+            "\"Hello\" + \"World\"");
+        var lexer = new GASLexer(inputStream);
+        var tokenStream = new CommonTokenStream(lexer);
+        var parser = new GASParser(tokenStream);
+        var expression = visitor.VisitExpression(parser.expression()) as Expression;
+        var scope = new Scope(null, null);
+        var result = interpreter.EvaluateExpression(expression, scope);
+
+
+        Assert.NotNull(result);
+        Assert.IsType<String>(result);
+        Assert.Equal("Hello World", result);
+    }
+    
+    [Fact]
+    public void PassEvaluateExpressionBinaryOpMinus()
+    {
+        var visitor = new ToAstVisitor();
+        var interpreter = new Interpreter();
+        var inputStream = new AntlrInputStream(
+            "7-5");
+        var lexer = new GASLexer(inputStream);
+        var tokenStream = new CommonTokenStream(lexer);
+        var parser = new GASParser(tokenStream);
+        var expression = visitor.VisitExpression(parser.expression()) as Expression;
+        var scope = new Scope(null, null);
+        var result = interpreter.EvaluateExpression(expression, scope);
+
+
+        Assert.NotNull(result);
+        Assert.IsType<float>(result);
+        Assert.Equal((float) 2, result);
+    }
+    
+    [Fact]
+    public void PassEvaluateExpressionBinaryOpMultiply()
+    {
+        var visitor = new ToAstVisitor();
+        var interpreter = new Interpreter();
+        var inputStream = new AntlrInputStream(
+            "3*5");
+        var lexer = new GASLexer(inputStream);
+        var tokenStream = new CommonTokenStream(lexer);
+        var parser = new GASParser(tokenStream);
+        var expression = visitor.VisitExpression(parser.expression()) as Expression;
+        var scope = new Scope(null, null);
+        var result = interpreter.EvaluateExpression(expression, scope);
+
+
+        Assert.NotNull(result);
+        Assert.IsType<float>(result);
+        Assert.Equal((float) 15, result);
+    }
+    
+}
---
+++ Tests/OperationalSemantics/InterpreterTests/EvaluateStatementTests/EvaluateStatementCanvas.cs
@@ -0,0 +1,51 @@
+﻿using Antlr4.Runtime;
+using GASLanguageProcessor;
+using GASLanguageProcessor.AST.Terms;
+using GASLanguageProcessor.FinalTypes;
+using GASLanguageProcessor.TableType;
+
+namespace Tests.OperationalSemantics.InterpreterTests.EvaluateStatementTests;
+
+public class EvaluateStatementCanvas
+{
+ 
+    [Fact]
+    public void PassEvaluateStatementCanvasWithColour()
+    {
+        var visitor = new ToAstVisitor();
+        var interpreter = new Interpreter();
+        var inputStream = new AntlrInputStream(
+            "canvas (250, 250, Colour(255, 255, 255, 1));");
+        var lexer = new GASLexer(inputStream);
+        var tokenStream = new CommonTokenStream(lexer);
+        var parser = new GASParser(tokenStream);
+        var statement = visitor.VisitCanvas(parser.canvas()) as Statement;
+        var scope = new Scope(null, null);
+        scope.vTable.Bind("canvas", new Variable("canvas", GasType.Canvas));
+        var result = interpreter.EvaluateStatement(statement, scope);
+
+
+        Assert.NotNull(result);
+        Assert.IsType<FinalCanvas>(result);
+    }
+    
+    [Fact]
+    public void PassEvaluateStatementCanvasWithoutColour()
+    {
+        var visitor = new ToAstVisitor();
+        var interpreter = new Interpreter();
+        var inputStream = new AntlrInputStream(
+            "canvas (250, 250);");
+        var lexer = new GASLexer(inputStream);
+        var tokenStream = new CommonTokenStream(lexer);
+        var parser = new GASParser(tokenStream);
+        var statement = visitor.VisitCanvas(parser.canvas()) as Statement;
+        var scope = new Scope(null, null);
+        scope.vTable.Bind("canvas", new Variable("canvas", GasType.Canvas));
+        var result = interpreter.EvaluateStatement(statement, scope);
+
+
+        Assert.NotNull(result);
+        Assert.IsType<FinalCanvas>(result);
+    }
+}
