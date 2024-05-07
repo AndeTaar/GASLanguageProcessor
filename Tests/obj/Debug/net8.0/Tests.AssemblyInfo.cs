9222492b19808148424c4505606da5609381afe5
AndreasBM1 <113091983+AndreasBM1@users.noreply.github.com>
2024-05-07 10:14:29 +0200

    Mouse coolio again

--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3448da48a4eb6f00d0f5a673b60d9239137b7504")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c1e407015dafa23333eda9ce345ca084b20e625")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
