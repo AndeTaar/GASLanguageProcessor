8bc68082acf04770b8b89a562da06737116b315c
ThomasSteenAndersen <coolfatboy02@gmail.com>
2024-05-15 18:33:43 +0200

    Develop

--- GASLanguageProcessor/obj/Debug/net8.0/GASLanguageProcessor.AssemblyInfo.cs
+++ GASLanguageProcessor/obj/Debug/net8.0/GASLanguageProcessor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GASLanguageProcessor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db45732931396e973df368d1fa068f301d5ea1cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60b375ff62d6b98ff0e458a64a8260a71b534641")]
 [assembly: System.Reflection.AssemblyProductAttribute("GASLanguageProcessor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GASLanguageProcessor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db45732931396e973df368d1fa068f301d5ea1cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60b375ff62d6b98ff0e458a64a8260a71b534641")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
