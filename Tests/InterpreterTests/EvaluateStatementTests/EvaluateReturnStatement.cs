6c112afeb2b21415e865483b30bcee6872fe0922
Daniel <danbodker@gmail.com>
2024-05-20 14:53:55 +0200

    Compounds now stop if return is encountered

---
+++ Tests/InterpreterTests/EvaluateStatementTests/EvaluateReturnStatement.cs
@@ -0,0 +1,27 @@
+﻿using GASLanguageProcessor;
+using GASLanguageProcessor.TableType;
+
+namespace Tests.OperationalSemantics.InterpreterTests.EvaluateStatementTests;
+
+public class EvaluateReturnStatement
+{
+    [Fact]
+    public void PassReturnStopsCompound()
+    {
+        var scope = SharedTesting.GetInterpretedScope(
+            "canvas (250 * 2, 10 * 50, Color(255, 255, 255, 1));" +
+            "num test1;" +
+            "void testFunction() {" +
+            "test1 = 10;" +
+            "return test1;" +
+            "test1 = 20;" +
+            "}" +
+            "testFunction();"
+        );
+        var result = scope.vTable.LookUp("test1")?.ActualValue;
+
+        Assert.NotNull(result);
+        Assert.Equal(10f, result);
+        Assert.NotEqual(20f, result);
+    }
+}
