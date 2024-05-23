03a343ee66c02432562676e88612df636a0158c5
Daniel <danbodker@gmail.com>
2024-05-23 15:57:05 +0200

    dividebyzero

--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1f85345005b00fb056b6176a6a155729fdc3167")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e66e3dc0fa68b9374b5db58183b694e5264c440f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
