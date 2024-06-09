ee9aa88268d0bcc253158bbd62855909bb1f691f
ThomasSteenAndersen <coolfatboy02@gmail.com>
2024-06-09 13:13:48 +0200

    Version3

--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df4334a5dc4e6adb9639a19bb1d0a22ee28d7e40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff4b16e01e5f7503900bd9f99cec8824ba2436cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
