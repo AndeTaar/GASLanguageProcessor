39fea3903c256c09fc4549de97da602cba5bb064
Thomas Andersen <coolfatboy02@gmail.com>
2024-06-14 12:13:30 +0200

    Version 4.1 With configuration management

--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1682b5a5ed9b1991ce0ba6eb4be2ef3e7ee4c090")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aef7d68abb482e3f686bb16488f74f7e6a586b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
