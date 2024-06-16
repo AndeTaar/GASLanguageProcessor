c7dfd8b749d9150d3463620b6489de23e2e8b9c6
Thomas Andersen <coolfatboy02@gmail.com>
2024-06-16 17:15:27 +0200

    From radians to degrees

--- GASLanguageProcessor/obj/Debug/net8.0/GASLanguageProcessor.AssemblyInfo.cs
+++ GASLanguageProcessor/obj/Debug/net8.0/GASLanguageProcessor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GASLanguageProcessor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a777e63d53f68666ddbe6d42a29dafd5f999029")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22bbc6b5b5107ae45b7bd6027293bce73a38f4ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("GASLanguageProcessor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GASLanguageProcessor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
